270b467e17ef627929c6209be06da54c61a23b1e
dbabbj <dbabb@qxdev.com>
2021-08-30 15:18:28 -0600

    testing CI

--- test/js/sum.test.js
+++ test/js/sum.test.js
@@ -1,5 +1,5 @@
-const { sum } = require('../../www/js/sum')
+const { sum } = require("../../www/js/sum");
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3)
-})
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(4);
+});

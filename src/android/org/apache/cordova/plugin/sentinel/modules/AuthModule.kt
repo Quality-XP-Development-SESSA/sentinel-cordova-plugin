035fed5bfd137e74180e098ffb81abfa08f62268
Jaquilar <jaquilar@qxdev.com>
2023-11-22 10:25:59 -0600

    [Merge] SMP-939: Implementar Ktlint para el plugin de Cordova
    - Merged SMP-916: Separar los módulos del plugin en diferentes archivos

--- src/android/org/apache/cordova/plugin/sentinel/modules/AuthModule.kt
+++ src/android/org/apache/cordova/plugin/sentinel/modules/AuthModule.kt
@@ -1,12 +1,14 @@
 package org.apache.cordova.plugin.modules
 
+import com.qxdev.sentinel_sdk.SentinelProvider
+import com.qxdev.sentinel_sdk.cloud.data.auth.User
+import com.qxdev.sentinel_sdk.cloud.data.auth.UserData
+import com.qxdev.sentinel_sdk.di.Koin
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import org.apache.cordova.CallbackContext
-import org.apache.cordova.CordovaPlugin
-import org.json.JSONArray
 import org.koin.core.context.GlobalContext.startKoin
 
 class AuthModule {
@@ -14,82 +16,41 @@ class AuthModule {
     private val cloudProvider = SentinelProvider.cloudProvider
     private val authServices = cloudProvider.authCloudServices
 
-class AuthModule {
-   private val coroutineScope = CoroutineScope(Dispatchers.IO + Job())
+    init {
+        startKoin {
+            modules(
+                Koin.sentinelSDKModule("https://api-stage.sensys-iot.com"),
+            )
+        }
+    }
 
-   private val cloudProvider = SentinelProvider.cloudProvider
+    fun signIn(
+        username: String,
+        password: String,
+        callbackContext: CallbackContext,
+    ) {
+        coroutineScope.launch {
+            val responseResult =
+                kotlin.runCatching { authServices.login(User(username.trim(), password.trim())) }
 
-   private val authServices = cloudProvider.authCloudServices
+            val result = responseResult.getOrNull()
 
-   init {
-      startKoin { 
-         modules(
-            Koin.sentinelSDKModule("https://api-stage.sensys-iot.com")
-         ) 
-      }
-   }
-
-   fun signIn(username: String, password: String, callbackContext: CallbackContext) {
-
-      coroutineScope.launch {
-         val responseResult =
-               kotlin.runCatching { authServices.login(User(username.trim(), password.trim())) }
-
-         val result = responseResult.getOrNull()
-
-         if(result != null){
-            if(result.success){
-               callbackContext.success(result.data.toString())
+            if (result != null) {
+                if (result.success) {
+                    callbackContext.success(result.data.toString())
+                } else {
+                    callbackContext.error("Oops! Something went wrong")
+                }
             } else {
-               callbackContext.error("Oops! Something went wrong")
+                callbackContext.error("Request failed with Exception ${responseResult.exceptionOrNull()?.message}")
             }
-         }else{
-            callbackContext.error("Request failed with Exception ${responseResult.exceptionOrNull()?.message}")
-         }
-      }
-      
-   }
+        }
+    }
 
-   fun signOut(callbackContext: CallbackContext){
-      coroutineScope.launch {
-         val responseResult = 
-            kotlin.runCatching { authServices.logout() }
-
-         val result = responseResult.getOrNull()
-
-         if(result != null){
-            if(result.success){
-               callbackContext.success(result.data.toString())
-            } else {
-               callbackContext.error(result.errorCode.toString())
-            }
-         }else{
-            callbackContext.error("Request failed with Exception ${responseResult.exceptionOrNull()?.message}")
-         }
-      }
-   }
-
-   fun accountType(callbackContext: CallbackContext){
-      coroutineScope.launch {
-         val responseResult = 
-            kotlin.runCatching { authServices.getAccountType() }
-         val result = responseResult.getOrNull()
-
-         if(result != null){
-            callbackContext.success(result.toString())
-         } else {
-            callbackContext.error("Request failed with Exception ${responseResult.exceptionOrNull()?.message}")
-         }
-      }
-   }
-
-   fun recoverPassword(email: String, callbackContext: CallbackContext){
-      if (email == null){
-         callbackContext.error("error")
-      } else {
-         coroutineScope.launch {
-            val responseResult = 
-               kotlin.runCatching { authServices.changePassword(UserData(email.trim())) }
+    fun signOut(callbackContext: CallbackContext) {
+        coroutineScope.launch {
+            val responseResult =
+                kotlin.runCatching { authServices.logout() }
 
             val result = responseResult.getOrNull()
 
@@ -102,7 +63,46 @@ class AuthModule {
             } else {
                 callbackContext.error("Request failed with Exception ${responseResult.exceptionOrNull()?.message}")
             }
-         }
-      }
-   }
+        }
+    }
+
+    fun accountType(callbackContext: CallbackContext) {
+        coroutineScope.launch {
+            val responseResult =
+                kotlin.runCatching { authServices.getAccountType() }
+            val result = responseResult.getOrNull()
+
+            if (result != null) {
+                callbackContext.success(result.toString())
+            } else {
+                callbackContext.error("Request failed with Exception ${responseResult.exceptionOrNull()?.message}")
+            }
+        }
+    }
+
+    fun recoverPassword(
+        email: String,
+        callbackContext: CallbackContext,
+    ) {
+        if (email == null) {
+            callbackContext.error("error")
+        } else {
+            coroutineScope.launch {
+                val responseResult =
+                    kotlin.runCatching { authServices.changePassword(UserData(email.trim())) }
+
+                val result = responseResult.getOrNull()
+
+                if (result != null) {
+                    if (result.success) {
+                        callbackContext.success(result.data.toString())
+                    } else {
+                        callbackContext.error(result.errorCode.toString())
+                    }
+                } else {
+                    callbackContext.error("Request failed with Exception ${responseResult.exceptionOrNull()?.message}")
+                }
+            }
+        }
+    }
 }

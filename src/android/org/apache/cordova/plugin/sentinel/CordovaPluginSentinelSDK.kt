0c4b0b02acae1c2ffce4d74db519b7ca172a222d
Jaquilar <jaquilar@qxdev.com>
2023-11-27 16:07:19 -0600

    [Improvement] SMP-967: Integración del módulo de Wifi Discovery en el plugin de Cordova
    - Implemented all the modules into the plugin working
    
    - Implemented the Context to initialize WifiDicovery correctly
    
    - Fixed a wrong export name module into the plugin.xml

--- src/android/org/apache/cordova/plugin/sentinel/CordovaPluginSentinelSDK.kt
+++ src/android/org/apache/cordova/plugin/sentinel/CordovaPluginSentinelSDK.kt
@@ -1,17 +1,29 @@
 package org.apache.cordova.plugin.sentinel
 
 import org.apache.cordova.CallbackContext
+import org.apache.cordova.CordovaInterface
 import org.apache.cordova.CordovaPlugin
 import org.apache.cordova.plugin.sentinel.interfaces.ModuleDelegate
 import org.apache.cordova.plugin.sentinel.modules.AuthModule
+import org.apache.cordova.plugin.sentinel.modules.CustomersModule
+import org.apache.cordova.plugin.sentinel.modules.DevicesModule
 import org.apache.cordova.plugin.sentinel.modules.LocationModule
+import org.apache.cordova.plugin.sentinel.modules.WifiDiscovery
 import org.json.JSONArray
 
 class CordovaPluginSentinelSDK : CordovaPlugin() {
-    private val authModule = AuthModule()
+    private var appContext: Context? = null
+
+    override fun initialize(cordova: CordovaInterface) {
+        super.initialize(cordova)
+        appContext = cordova.activity?.applicationContext
+    }
 
     private val authModuleDelegate: ModuleDelegate = AuthModule()
+    private val customersModuleDelegate: ModuleDelegate = CustomersModule()
+    private val devicesModuleDelegate: ModuleDelegate = DevicesModule()
     private val locationModuleDelegate: ModuleDelegate = LocationModule()
+    private val wifiDiscoveryModuleDelegate: ModuleDelegate = WifiDiscovery(appContext)
 
     override fun execute(
         action: String,
@@ -22,7 +34,10 @@ class CordovaPluginSentinelSDK : CordovaPlugin() {
 
         return when (module) {
             "AuthModule" -> authModuleDelegate.executeAction(actionExec, args, callbackContext)
+            "CustomersModule" -> customersModuleDelegate.executeAction(actionExec, args, callbackContext)
+            "DevicesModule" -> devicesModuleDelegate.executeAction(actionExec, args, callbackContext)
             "LocationModule" -> locationModuleDelegate.executeAction(actionExec, args, callbackContext)
+            "WifiDiscovery" -> wifiDiscoveryModuleDelegate.executeAction(actionExec, args, callbackContext)
             else -> false
         }
     }
